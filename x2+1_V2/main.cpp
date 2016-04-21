f7382b7ed0d1ef177b829518f9d1deba5d472d77
adarsh kesireddy <kesireddy.adarsh.k@gmail.com>
2016-04-20 22:12:10 -0700

    Initial Commit

---
+++ x2+1_V2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  x2+1_V2
+//
+//  Created by adarsh kesireddy on 4/20/16.
+//  Copyright © 2016 adarsh kesireddy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
